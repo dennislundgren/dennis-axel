7f63138033ea76b131ce6b50e0adb72f22b61743
Dennis Lundgren <dennis.axel@icloud.com>
2024-10-26 22:52:55 +0200

    feat: update git config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <header>Dennis</header>
+      <header>Dennis Axel</header>
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
         <p>
           Just nu jobbar jag på{" "}
