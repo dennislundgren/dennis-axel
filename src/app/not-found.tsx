3c76bf0ea0ce19b4fe7646e0726f37fc6f174ef9
Dennis Lundgren <dennis.axel@icloud.com>
2024-10-27 23:33:14 +0100

    feat: 404 styling update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,18 +4,11 @@ import React from "react";
 
 const Custom404 = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
+    <main className="flex min-h-full-dynamic flex-col items-center justify-center p-4 lg:p-24 gap-8">
       <Card>
         <Body>Sorry, the page you are looking for does not exist.</Body>
       </Card>
-    </div>
+    </main>
   );
 };
 
