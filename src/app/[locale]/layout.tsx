1f3107ba1c44a33a5f746a0dc4e141584001c980
Dennis Lundgren <dennis.axel@icloud.com>
2025-06-25 16:24:17 +0200

    enable static rendering with [locale]page - v0.2.11

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -22,7 +22,6 @@ export async function generateMetadata({
   params: Promise<{ locale: Locales }>;
 }): Promise<Metadata> {
   const { locale } = await params;
-  setRequestLocale(locale);
   const t = await getTranslations({ locale, namespace: "Metadata" });
 
   return {
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,14 @@
 import Content from "@/components/Content";
 import CurrentlyPlaying from "@/components/CurrentlyPlaying";
+import { setRequestLocale } from "next-intl/server";
 
-export default function Home() {
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  setRequestLocale(locale);
   return (
     <main className="flex min-h-full-dynamic flex-col items-center justify-center p-4 lg:p-24 gap-8">
       <Content />
