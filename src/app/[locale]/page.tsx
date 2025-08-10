cf9e0a1f21bbc8aecf0683ca517295c63ea9183c
Dennis Lundgren <dennis.axel@icloud.com>
2025-08-10 13:10:55 +0200

    suspence content

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,9 @@
 import CardContent from "@/components/CardContent";
 import CurrentlyPlaying from "@/components/CurrentlyPlaying";
+import Card from "@/components/UI/Card";
+import { Body } from "@/components/UI/Texts";
 import { setRequestLocale } from "next-intl/server";
+import { Suspense } from "react";
 
 interface Props {
   params: Promise<{ locale: string }>;
@@ -14,8 +17,16 @@ export default async function Home({ params }: Props) {
   return (
     <main className="flex min-h-full-dynamic flex-col items-center justify-center p-4 lg:p-24 gap-8">
       <div className="relative">
-        <CardContent />
-        <CurrentlyPlaying />
+        <Suspense
+          fallback={
+            <Card>
+              <Body>Loading</Body>
+            </Card>
+          }
+        >
+          <CardContent />
+          <CurrentlyPlaying />
+        </Suspense>
       </div>
     </main>
   );
